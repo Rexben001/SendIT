cf129b75afbdbbf37e68c95385dfcec94992648a
Rexben <rexben.rb@gmail.com>
2018-10-29 23:24:45 +0100

    added transitioning

--- UI/javascript/sign-in.js
+++ UI/javascript/sign-in.js
@@ -1,4 +1,18 @@
+let signIn = document.getElementById('formSignIn');
+let signUp = document.getElementById('formSignUp');
+
+
 document.getElementById('signIn').addEventListener('click', function(){
-let signIn = document.getElementsById('formSignIn');
+    let signIn = document.getElementById('formSignIn');
+let signUp = document.getElementById('formSignUp');
 signIn.style.display = "none";
+signUp.style.display = "block";
 });
+
+document.getElementById('signUp').addEventListener('click', function(){
+    let signIn = document.getElementById('formSignIn');
+    let signUp = document.getElementById('formSignUp');
+    signUp.style.display = "none";
+    signIn.style.display = "block";
+    console.log('hey')
+    });
