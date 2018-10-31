9419c1b93b48ec33658518fb285bff3e0c697e6f
Rexben <rexben.rb@gmail.com>
2018-10-31 21:06:43 +0100

    Made the website interactive

--- UI/javascript/sign-in.js
+++ UI/javascript/sign-in.js
@@ -1,18 +1,24 @@
-let signIn = document.getElementById('formSignIn');
-let signUp = document.getElementById('formSignUp');
+const signIn = document.getElementById('formSignIn');
+const signUp = document.getElementById('formSignUp');
 
 
 document.getElementById('signIn').addEventListener('click', function(){
-    let signIn = document.getElementById('formSignIn');
-let signUp = document.getElementById('formSignUp');
+   
 signIn.style.display = "none";
 signUp.style.display = "block";
 });
 
 document.getElementById('signUp').addEventListener('click', function(){
-    let signIn = document.getElementById('formSignIn');
-    let signUp = document.getElementById('formSignUp');
     signUp.style.display = "none";
     signIn.style.display = "block";
     console.log('hey')
     });
+
+signUp.addEventListener('submit', function(){
+    let password = document.getElementById('password');
+    let confirmPassword = document.getElementById('confirm-password');
+
+    if(confirmPassword.value !== password.value){
+        alert('Password does not match');
+    }
+});
