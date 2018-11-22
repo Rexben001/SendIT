048460e8f7538ef4c6b9238e23a461846055e83c
Rexben001 <rexben.rb@gmail.com>
2018-11-22 10:51:17 +0100

    Successfully hashed and compare user's password

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -32,85 +32,110 @@ class UserController {
 
 	static addUser(req, res) {
 
-		const user = {
-			firstname: req.body.firstname,
-			lastname: req.body.lastname,
-			othernames: req.body.othernames,
-			username: req.body.username,
-			email: req.body.email,
-			phone: req.body.phone,
-			password: bcrypt.hash(req.body.password, 10),
-			isAdmin: req.body.isAdmin,
-			
-		};
-		pool.connect((err, client, done) => {
-			const query = 'INSERT INTO users(firstname, lastname, othernames, username, email, phone, password, isAdmin, registered) VALUES($1,$2,$3,$4,$5,$6,$7,$8,NOW()) RETURNING *';
-			const values = [user.firstname, user.lastname, user.othernames, user.username, user.email, user.phone, user.password, user.isAdmin];
+		bcrypt.hash(req.body.password, 10, (err, hash) => {
 
-			client.query(query, values, (error, result) => {
-				done();
-				if (error) {
-					return res.status(400).json({ error });
-				}
-				return res.status(200).send({
-					status: 'Successful',
-					result: result.rows,
+			if (err) {
+				return res.json({
+					message: 'Unable to hash password'
+				})
+			}
+			const user = {
+				firstname: req.body.firstname,
+				lastname: req.body.lastname,
+				othernames: req.body.othernames,
+				username: req.body.username,
+				email: req.body.email,
+				phone: req.body.phone,
+				password: hash,
+				isAdmin: req.body.isAdmin,
+
+			};
+			pool.connect((err, client, done) => {
+				const query = 'INSERT INTO users(firstname, lastname, othernames, username, email, phone, password, isAdmin, registered) VALUES($1,$2,$3,$4,$5,$6,$7,$8,NOW()) RETURNING *';
+				const values = [user.firstname, user.lastname, user.othernames, user.username, user.email, user.phone, user.password, user.isAdmin];
+
+				client.query(query, values, (error, result) => {
+					done();
+					if (error) {
+						return res.status(400).json({ error });
+					}
+					return res.status(200).send({
+						status: 'Successful',
+						result: result.rows,
+					});
 				});
 			});
-		});
+		})
+
 	}
 
 	static loginUser(req, res) {
 		const password = req.body.password;
-		const data = req.body.name;
+		const data = req.body.username;
 		pool.connect((err, client, done) => {
-			const query = `SELECT * FROM users WHERE name=$1 AND password=$2`;
-			const value = [data, password];
+			const query = `SELECT password FROM users WHERE username=$1`;
+			const value = [data];
 			client.query(query, value, (error, result) => {
 				done();
-				if(err){
-					return res.status(400).json({ err });
-				}
-				if (error) {
-					console.log(error);
-					return res.status(400).json({ error });
-				} else {
-					return res.status(202).send({
-						status: 'Successful',
-						result: result,
+				if (result.rowCount === 0) {
+					return res.json({
+						message: 'Pls, enter a valid username'
 					});
+
+				}
+				else {
+					bcrypt.compare(password, result.rows[0].password, (err, result) => {
+						console.log(result);
+						if (err) {
+							return res.json({ err });
+						}
+						if (error) {
+							console.log(error);
+							return res.json({ error });
+						} else {
+							return res.json({
+								status: 'Successful',
+								result: 'Welcome to Send It'
+							});
+						}
+					});
+
 				}
 			});
 		});
 	}
+
 
 
 
 	static userParcel(req, res) {
-		const id = req.params.user_id;
-		pool.connect((err, client, done) => {
-			const query = `SELECT * FROM parcels WHERE user_id=${id}`;
-			client.query(query, (error, result) => {
-				done();
-				if (error) {
-					return res.status(400).json({
-						result: "An error occurred",
-						error,
-					});
-				} else
-				if (result.rowCount === 0) {
-					res.status(404).send({
-						status: 'Failed',
-						message: 'No users information found',
+					const id = req.params.user_id;
+					pool.connect((err, client, done) => {
+						const query = `SELECT * FROM parcels WHERE user_id=${id}`;
+						client.query(query, (error, result) => {
+							done();
+							if (error) {
+								return res.json({
+									result: "An error occurred",
+									error,
+								});
+							} else{
+								if (result.rowCount === 0) {
+									return res.json({
+										status: 'Failed',
+										message: 'No users information found',
+									});
+								}
+							return res.json({
+								status: 'success',
+								result: result.rows
+							});
+						}
+						});
 					});
 				}
-				return res.json({
-					status: 'success',
-					result: result.rows
-				});
-			});
-		});
-	}
 }
+
+
 
 export default UserController;
